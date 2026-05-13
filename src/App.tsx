44b8636df18c99a4ec126ee1b67307b862ba4a2f
Seengar <seengar237@gmail.com>
2026-05-13 15:22:59 +0500

    fix: correct invalid import identifier My-groups

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home'
 import Create from './pages/Create'
-import My-groups from './pages/My-groups'
+import MyGroups from './pages/My-groups'
 import Discover from './pages/Discover'
 export default function App() {
   return (
@@ -9,7 +9,7 @@ export default function App() {
       <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create" element={<Create />} />
-          <Route path="/my-groups" element={<My-groups />} />
+          <Route path="/my-groups" element={<MyGroups />} />
           <Route path="/discover" element={<Discover />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
